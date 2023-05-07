1b12d24ca6f6c7c545b76f5864b175a19945ac4c
LynnArsa <lintang13arsa@gmail.com>
2023-05-08 00:13:09 +0700

    Update

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleDropdown1() {
+    const dropdown = document.getElementById('dropdown1');
+    dropdown.classList.toggle('hidden');
+}
+
+function toggleDropdown2() {
+    const dropdown = document.getElementById('dropdown2');
+    dropdown.classList.toggle('hidden');
+}
+
+function upgradePage() {
+    window.location.href = "upgrade.html";
+}
+
+function homePage() {
+    window.location.href = "index.html";
+}
